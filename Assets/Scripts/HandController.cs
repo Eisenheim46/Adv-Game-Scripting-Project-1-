a078baf10b9d3db484bb3f7533f3c8e3d8e0dfe4
Anthony Flores <Anthony.Flores2@iam.local>
2018-02-20 18:32:51 -0600

    Menu Place Holders Added
    Made the menu skeleton. Some fonts and colors

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+
+    private Rigidbody player; //rigid body of the player (hand)
+
+    //For Player Movements -----
+    [SerializeField] private float speed; //speed of the player (hand) movements
+    [SerializeField] private Vector2 jumpArch;
+
+
+    private float horizontalInput; //to get the player input ("Horizontal")
+    private float verticalInput; //to get the player input ("Vertical")
+
+    private bool moveEnabled; //To allow movement or not
+    private bool isGrounded; //Check if on the ground *Avoid double jump 
+    //-----
+
+    
+
+	// Use this for initialization
+	void Awake ()
+	{
+        player = GetComponent<Rigidbody>(); //get the rigidbody component before using it
+       
+	}
+
+    private void Start()
+    {
+        moveEnabled = true;
+        isGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update ()
+	{
+        if (player.velocity.y == 0)
+        {
+            moveEnabled = true;
+            isGrounded = true;
+        }
+
+
+        if (moveEnabled)
+            Move();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+            JumpSlap();
+       
+	}
+
+
+    private void Move()
+    {
+        if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+        {
+            horizontalInput = Input.GetAxis("Horizontal");
+            verticalInput = Input.GetAxis("Vertical");
+
+            player.velocity = new Vector3(horizontalInput, 0, verticalInput) * speed;
+        }
+    }
+
+    private void JumpSlap()
+    {
+        moveEnabled = false;
+        isGrounded = false;
+
+        player.velocity = new Vector3(jumpArch.x, jumpArch.y);
+
+    }
+
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    private int arenaSceneIndex = 1;
+
+    public void ClickedStartButton()
+    {
+        SceneManager.LoadScene(arenaSceneIndex);
+    }
+
+    public void ClickedControlsButton()
+    {
+
+    }
+
+    public void ClickedQuitButton()
+    {
+        Application.Quit();
+    }
+
+}
