1ebc634e017871e02c90b693be97d6c2875b5ce8
Anthony Flores <Anthony.Flores2@iam.local>
2018-03-05 15:22:00 -0600

    Updated Scripts

--- Assets/Scripts/HandController.cs
+++ Assets/Scripts/HandController.cs
@@ -5,8 +5,18 @@ using UnityEngine;
 public class HandController : MonoBehaviour
 {
 
+    // Player Properties ----
     [SerializeField] private int playerNumber;
+
+    [SerializeField] private float coolDownSeconds; //Cool down in seconds before moving. Mainly for Slip()
+    private float stampedTime; // To caputured time the moment it is used.
+    private float groundDistance; //To check how far the player is from the ground
+
+    private Collider collider; //To get player's collider component
     private Rigidbody player; //rigid body of the player (hand)
+
+    //-----
+
 
     //For Player Inputs -----
     private float horizontalInput; //to get the player input ("Horizontal")
@@ -15,25 +25,38 @@ public class HandController : MonoBehaviour
     private string horizontalButtons;
     private string verticalButtons;
     private string jumpButton;
-
+    private string fireButton;
     //-----
 
 
     //For Player Movements -----
     [SerializeField] private float speed; //speed of the player (hand) movements
-    [SerializeField] private Vector2 jumpArch;
+    [SerializeField] private float slipOffsetAmount;//How far to push the player back shen slipping
+
+    [SerializeField] private Vector2 jumpArch; //How far x and how far y when jumping
+
+    private Vector3 slipOffset; //To convert the slipOffsetAmount into a Vector 3 later;
 
     private bool moveEnabled; //To allow movement or not
     private bool isGrounded; //Check if on the ground *Avoid double jump 
     //-----
 
-    
+    //Properties -----
+    private bool IsGrounded
+    {
 
-	// Use this for initialization
-	void Awake ()
+        get { return Physics.Raycast(transform.position, -Vector3.up, groundDistance + 0.01f); }
+
+    }
+
+    //-----
+
+
+    // Use this for initialization
+    void Awake ()
 	{
         player = GetComponent<Rigidbody>(); //get the rigidbody component before using it
-       
+        collider = GetComponent<Collider>();//Get the player's collider component.
 	}
 
     private void Start()
@@ -44,20 +67,27 @@ public class HandController : MonoBehaviour
         horizontalButtons = "Horizontal" + playerNumber;
         verticalButtons = "Vertical" + playerNumber;
         jumpButton = "Jump" + playerNumber;
+        fireButton = "Fire" + playerNumber;
+
+        groundDistance = collider.bounds.extents.y;
     }
 
     // Update is called once per frame
     void Update ()
 	{
+        EnableMovement();  //Will enable movement
 
-        CheckGrounded();  //Check if on the ground. Will enable movement
-
-        if (moveEnabled)
+        if (moveEnabled) //Move is enabled when they are not jumping and after they slipped
             Move();
 
-        if (Input.GetButtonDown(jumpButton) && isGrounded)
+        if (Input.GetButtonDown(jumpButton) && isGrounded) //Jump when on the ground. Not while in the air.
             JumpSlap();
-       
+
+        if (Input.GetButtonDown(fireButton) && isGrounded && moveEnabled) //Slip should only happen on the ground
+            Slip();
+
+
+        Debug.Log(player.velocity.y);
 	}
 
 
@@ -82,15 +112,46 @@ public class HandController : MonoBehaviour
 
     }
 
-    private void CheckGrounded()
+    private void Slip()
+    {
+
+        moveEnabled = false;
+
+        stampedTime = Time.time + coolDownSeconds;
+
+        switch (playerNumber)
+        {
+            case 1:
+                slipOffset = new Vector3(slipOffsetAmount, 0, 0);
+                player.MovePosition(transform.position - slipOffset); //Slip to the left
+
+                break;
+            case 2:
+                slipOffset = new Vector3(slipOffsetAmount, 0, 0);
+                player.MovePosition(transform.position + slipOffset); //Slip to the Right
+
+                break;
+            case 3: break;
+            case 4: break;
+        }
+
+    }
+
+
+    private void EnableMovement()
     {
 
 
-        if (player.velocity.y == 0) //Will be refined by another technique
+        if (IsGrounded) //Check if on the ground.
         {
-            moveEnabled = true;
             isGrounded = true;
+
+            if (stampedTime <= Time.time) //Check if cooldown is complete.
+                moveEnabled = true;
         }
+
     }
+
+
 
 }
